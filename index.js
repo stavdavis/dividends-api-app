8bf46db76a999b7ea14a8ef67a15595fab05fb44
Stav Davis <stav.davis@gmail.com>
2017-11-16 15:29:37 -0800

    Added error handling and nested all getJSON functions so an error in one will stop the others from loading

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 // First we get the dividend amount and date, (using source #1 and getJSON 
 //(NEED TO ADD ERROR MSG, IF NO DIVIDEND)
 function getLatestDividendData(stockSymbol) {
-  $('.wait-message').text('');  //Clear the wait message
   let dividendJsonUrl = `https://api.iextrading.com/1.0/stock/${stockSymbol}/dividends/3m`;
   let stockQuoteJsonUrl = `https://api.iextrading.com/1.0/stock/${stockSymbol}/chart/1y`;
   $.getJSON(dividendJsonUrl, data1 => {
@@ -19,41 +18,42 @@ function getLatestDividendData(stockSymbol) {
       $('.dividend-results-detail').append(`<br>${stockSymbol.toUpperCase()}'s stock quote on that date was $${quoteOnDividendDate}<br>`);
       $('.dividend-results-main').html(`<br>That represents an annualized return rate of:<br>${annualizedDividend}%<br>`);
       $('.top-bar-title').text(`${stockSymbol.toUpperCase()}'s annualized dividend rate: ${annualizedDividend}%:`);
-      $('.top-stock-dividend').width(Math.min(annualizedDividend, 100) + 'vw')//'vw' = screen width
+      $('.top-stock-dividend').width(Math.min(annualizedDividend, 100) + '%')
                               .css("background-color", 'green')
                               .css("border", '1px solid black');
       $('.results-text').css("border", "1px solid black");
+      $('.results-bar-chart').css("border", "1px solid black");
+      getOneYearStockGains(stockSymbol);
+      getOneYearSP500Gains(); //uses Vanguard's S&P500 fund - symbol: 'VOO'
     });
-  });  
+  }).error(function() { alert(`Error fetching data.\nMake sure the stock symbol is valid - check out the example buttons below.`) });  
 }
 
 function getOneYearStockGains(stockSymbol) {
-  $('.wait-message').text('');  //Clear the wait message
   let stockQuoteJsonUrl = `https://api.iextrading.com/1.0/stock/${stockSymbol}/chart/1y`;
   $.getJSON(stockQuoteJsonUrl, data2 => {
     let latestStockQuote = data2[data2.length-1].close; //the last element in the 1y array of objects
     let stockQuoteOneYearBack = data2[0].close; //the first element in the 1y array of objects
     let annualGainNonDividend = ((100 * (latestStockQuote - stockQuoteOneYearBack) / stockQuoteOneYearBack)).toFixed(2);
     $('.non-dividend-results').html(`<br>Compare that to ${stockSymbol.toUpperCase()}'s non-dividend gains over 
-      the past year (updatad today):<br>${annualGainNonDividend}%`); 
+      the past year (updated today):<br>${annualGainNonDividend}%`); 
     $('.middle-bar-title').text(`${stockSymbol.toUpperCase()}'s 1-year gain (non-dividend): ${annualGainNonDividend}%:`);
-    $('.middle-stock-non-dividend').width(Math.min(annualGainNonDividend, 100) + 'vw')//'vw' = screen width
+    $('.middle-stock-non-dividend').width(Math.min(annualGainNonDividend, 100) + '%')
                                   .css("background-color", 'darkorange')
                                   .css("border", '1px solid black');
   });
 }
 
 function getOneYearSP500Gains() {
-  //uses Vanguard's S&P500 fund - symbol: 'VOO'
   let sp500QuoteJsonUrl = `https://api.iextrading.com/1.0/stock/VOO/chart/1y`; 
   $.getJSON(sp500QuoteJsonUrl, data3 => {
     let latestSP500Quote = data3[data3.length-1].close; //the last element in the 1y array of objects
     let sp500QuoteOneYearBack = data3[0].close; //the first element in the 1y array of objects
     let sp500AnnualGain = ((100 * (latestSP500Quote - sp500QuoteOneYearBack) / sp500QuoteOneYearBack)).toFixed(2);
     $('.sp500-results').html(`<br>Also, compare that to the S&P-500 gains over 
-      the past year (updatad today):<br>${sp500AnnualGain}%`);  
+      the past year (updated today):<br>${sp500AnnualGain}%`);  
     $('.bottom-bar-title').text(`S&P-500's 1-year gain: ${sp500AnnualGain}%:`);
-    $('.bottom-sp500').width(Math.min(sp500AnnualGain, 100) + 'vw') //'vw' = screen width
+    $('.bottom-sp500').width(Math.min(sp500AnnualGain, 100) + '%') 
                       .css("background-color", 'purple')
                       .css("border", '1px solid black');
   });
@@ -65,8 +65,6 @@ function clearAllResultsFields() {
   $('.bottom-bar-title').text(``);
 
   $('.bar').css({"border":"0","background-color":"white"});
-  // $('.middle-stock-non-dividend').css({"border":"0","background-color":"0"});
-  // $('.bottom-sp500').css({"border":"0","background-color":"0"});
   
   $('.top-stock-dividend').html(``);
   $('.middle-stock-non-dividend').html(``);
@@ -87,8 +85,6 @@ function listenForFreeTextSubmission() {
     const userInput = $(event.currentTarget).find('.stock-symbol-input');
     const userStockSymbol = userInput.val();
     getLatestDividendData(userStockSymbol);
-    getOneYearStockGains(userStockSymbol);
-    getOneYearSP500Gains(); //uses Vanguard's S&P500 fund - symbol: 'VOO'
   });
 }
 
@@ -98,11 +94,10 @@ function listenForFixedButtonSubmission() {
     event.preventDefault();
     const userStockSymbol = $(event.currentTarget).text();
     getLatestDividendData(userStockSymbol);
-    getOneYearStockGains(userStockSymbol);
-    getOneYearSP500Gains(); //uses Vanguard's S&P500 fund - symbol: 'VOO'
   });
 }
 
+let errorFlag = 0;
 $(listenForFreeTextSubmission);
 $(listenForFixedButtonSubmission);
 
