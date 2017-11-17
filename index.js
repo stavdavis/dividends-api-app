f6c7a17989113e6e5ea084016bdf07e9154936ba
Stav Davis <stav.davis@gmail.com>
2017-11-17 00:10:26 -0800

    added dynamic formatting for different screen sizes

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ function getLatestDividendData(stockSymbol) {
   $.getJSON(dividendJsonUrl, data1 => {
     let dividendAmount = data1[0].amount;  
     let dividendDate = data1[0].declaredDate; 
-    $('.dividend-results-detail').html(`${stockSymbol.toUpperCase()}'s most recent quarterly dividend amount was:<br>$${dividendAmount}<br>
+    $('.dividend-results-detail').html(`${stockSymbol.toUpperCase()}'s most recent quarterly dividend amount was: $${dividendAmount}<br>
       <br>
-      ${stockSymbol.toUpperCase()} declared that dividend on:<br>${dividendDate}<br>`);
+      ${stockSymbol.toUpperCase()} declared that dividend on: ${dividendDate}<br>`);
     //Now we need another API call to get the stock quote on the dividend date (uses dividendDate, so has to be nested)
     $.getJSON(stockQuoteJsonUrl, data2 => {
       let quoteOnDividendDate = data2.find(item => item.date == dividendDate).close;
       let annualizedDividend = (4 * 100 * dividendAmount / quoteOnDividendDate).toFixed(2);
-      $('.dividend-results-detail').append(`<br>${stockSymbol.toUpperCase()}'s stock quote on that date was $${quoteOnDividendDate}<br>`);
-      $('.dividend-results-main').html(`<br>That represents an annualized return rate of:<br>${annualizedDividend}%<br>`);
-      $('.top-bar-title').text(`${stockSymbol.toUpperCase()}'s annualized dividend rate: ${annualizedDividend}%:`);
+      $('.dividend-results-detail').append(`<br>${stockSymbol.toUpperCase()}'s stock quote on that date was:<br>$${quoteOnDividendDate}<br>`);
+      $('.dividend-results-main').html(`<br>That represents an annualized return rate of: ${annualizedDividend}%<br>`);
+      $('.top-bar-title').text(`${stockSymbol.toUpperCase()}'s annualized dividend rate: ${annualizedDividend}%`);
       $('.top-stock-dividend').width(Math.min(annualizedDividend, 100) + '%')
                               .css("background-color", 'green')
                               .css("border", '1px solid black');
@@ -36,8 +36,8 @@ function getOneYearStockGains(stockSymbol) {
     let stockQuoteOneYearBack = data2[0].close; //the first element in the 1y array of objects
     let annualGainNonDividend = ((100 * (latestStockQuote - stockQuoteOneYearBack) / stockQuoteOneYearBack)).toFixed(2);
     $('.non-dividend-results').html(`<br>Compare that to ${stockSymbol.toUpperCase()}'s non-dividend gains over 
-      the past year (updated today):<br>${annualGainNonDividend}%`); 
-    $('.middle-bar-title').text(`${stockSymbol.toUpperCase()}'s 1-year gain (non-dividend): ${annualGainNonDividend}%:`);
+      the past year (updated today): ${annualGainNonDividend}%`); 
+    $('.middle-bar-title').text(`${stockSymbol.toUpperCase()}'s 1-year gain (non-dividend): ${annualGainNonDividend}%`);
     $('.middle-stock-non-dividend').width(Math.min(annualGainNonDividend, 100) + '%')
                                   .css("background-color", 'darkorange')
                                   .css("border", '1px solid black');
@@ -51,8 +51,8 @@ function getOneYearSP500Gains() {
     let sp500QuoteOneYearBack = data3[0].close; //the first element in the 1y array of objects
     let sp500AnnualGain = ((100 * (latestSP500Quote - sp500QuoteOneYearBack) / sp500QuoteOneYearBack)).toFixed(2);
     $('.sp500-results').html(`<br>Also, compare that to the S&P-500 gains over 
-      the past year (updated today):<br>${sp500AnnualGain}%`);  
-    $('.bottom-bar-title').text(`S&P-500's 1-year gain: ${sp500AnnualGain}%:`);
+      the past year (updated today): ${sp500AnnualGain}%`);  
+    $('.bottom-bar-title').text(`S&P-500's 1-year gain: ${sp500AnnualGain}%`);
     $('.bottom-sp500').width(Math.min(sp500AnnualGain, 100) + '%') 
                       .css("background-color", 'purple')
                       .css("border", '1px solid black');
