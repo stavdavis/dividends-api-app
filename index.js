0e4c02e05da1e53427f73814e6208408f45e1497
Stav Davis <stav.davis@gmail.com>
2017-11-17 20:22:22 -0800

    added lots of formatting; shortened all the longass variable names and function names

--- index.js
+++ index.js
@@ -7,11 +7,11 @@
 //(4) Then it will run another getJson to get the S&P500 quotes today and 1 yr back.
 //(5) Then, it will render all the bar charts and textual summary for the dividend, the stock and the S&P-500.
 //(6) Finally, it will alert the user if an invalid stock symbol was entered. 
-function getDataAndRenderSummary(stockSymbol) {
+function getDataRenderSummary(stockSymbol) {
   let dividendJsonUrl = makeDividendJsonUrl(stockSymbol);
   let stockQuoteJsonUrl = makeStockJsonUrl(stockSymbol);
   $.getJSON(dividendJsonUrl, data1 => {
-    extractDataAndRenderSummary(data1, stockSymbol, stockQuoteJsonUrl)
+    renderSummary(data1, stockSymbol, stockQuoteJsonUrl)
   }).error(e => { jsonErrorAlert(e) });  
 }
 
@@ -23,42 +23,45 @@ function makeStockJsonUrl(stockSymbol) {
   return `https://api.iextrading.com/1.0/stock/${stockSymbol}/chart/1y`;
 }
 
-function extractDataAndRenderSummary(data1, stockSymbol, stockQuoteJsonUrl) {
+//This function extracts the data and renders the summary text and bar charts
+function renderSummary(data1, stockSymbol, stockQuoteJsonUrl) {
   //first we do error handling: if this stock symbol is valid, but does not bear dividends:
   if (data1[0] == undefined) { alert('This stock symbol is valid, but it does not bear dividends. Try another one!')}
   //otherwise, extract data and render results:
   else {
     let dividendAmount = data1[0].amount;  
     let dividendDate = data1[0].declaredDate; 
-    renderSummaryText_DividendAmountAndDate(stockSymbol, dividendAmount,dividendDate);
+    renderText_DivAmtDate(stockSymbol, dividendAmount,dividendDate);
     //Now we need another API call to get the stock quote on the dividend date (uses dividendDate, so has to be nested)
-    getStockJsonAndRenderDividendSummaryAndCharts(stockQuoteJsonUrl,stockSymbol, dividendDate, dividendAmount);  
+    loadDivTextCharts(stockQuoteJsonUrl,stockSymbol, dividendDate, dividendAmount);  
     renderLogo(stockSymbol);
   }
 }
 
-function getStockJsonAndRenderDividendSummaryAndCharts(stockQuoteJsonUrl,stockSymbol, dividendDate, dividendAmount) {
-  $.getJSON(stockQuoteJsonUrl, data2 => renderDividendSummaryTextAndBarCharts(data2, stockSymbol, dividendDate, dividendAmount));  
+//Get the dividend Json and render the summary text and bar charts for the dividend data
+function loadDivTextCharts(stockQuoteJsonUrl,stockSymbol, dividendDate, dividendAmount) {
+  $.getJSON(stockQuoteJsonUrl, data2 => { renderDivTextCharts(data2, stockSymbol, dividendDate, dividendAmount)});  
 }
 
-function renderDividendSummaryTextAndBarCharts(data2, stockSymbol, dividendDate, dividendAmount) {
-  let quoteOnDividendDate = extractStockQuoteOnDividendDate(data2, dividendDate);
-  let annualizedDividend = annualizeDividend(dividendAmount, quoteOnDividendDate);
-  renderSummaryText_DividendAnnualizedRate(stockSymbol, quoteOnDividendDate, annualizedDividend)
-  renderDividendBarChartAndTitle(stockSymbol, annualizedDividend);
-  renderBorders_resultsTextAndBarChartAreas();
+//Used by previous function - this part does the actual rendering of the dividend summary text and charts
+function renderDivTextCharts(data2, stockSymbol, dividendDate, dividendAmount) {
+  let quoteOnDivDate = getPriceOnDivDate(data2, dividendDate);
+  let annualizedDiv = annualizeDividend(dividendAmount, quoteOnDivDate);
+  renderDivRateText(stockSymbol, quoteOnDivDate, annualizedDiv)
+  renderDivChart(stockSymbol, annualizedDiv);
+  renderChartAreaBrdrs();
   //Now we get the non-dividend stock data, using another getJson
   getOneYearStockGains(stockSymbol);
   //And finally, we get the S&P500 baseline data, using another getJson
   getOneYearSP500Gains(); //uses Vanguard's S&P500 fund - symbol: 'VOO'
 }
 
-function extractStockQuoteOnDividendDate(jsonData, dividendDate) {
+function getPriceOnDivDate(jsonData, dividendDate) {
   return jsonData.find(item => (item.date == dividendDate)).close;
 }
 
-function annualizeDividend(dividendAmount, quoteOnDividendDate) {
-  return (4 * 100 * dividendAmount / quoteOnDividendDate).toFixed(2);
+function annualizeDividend(dividendAmount, quoteOnDivDate) {
+  return (4 * 100 * dividendAmount / quoteOnDivDate).toFixed(2);
 }
 
 //Error handling - there are two primary errors:
@@ -68,73 +71,76 @@ function jsonErrorAlert(error) {
   alert(`This is not a valid stock symbol.\nTry a different one, or you can try the example buttons below.`) 
 }
 
-function renderBorders_resultsTextAndBarChartAreas() {
-  $('.results-text').css("border", "1px solid black");
+function renderChartAreaBrdrs() {
+  // $('.results-text').css("border", "0px solid black");
   $('.results-bar-chart').css("border", "1px solid black");
 }
 
-function renderDividendBarChartAndTitle(stockSymbol, annualizedDividend) {
-  $('.top-bar-title').text(`${stockSymbol.toUpperCase()}'s annualized dividend rate: ${annualizedDividend}%`);
-  $('.top-stock-dividend').width(Math.min(annualizedDividend, 100) + '%')
-                          .css("background-color", 'green')
-                          .css("border", '1px solid black');
+//Renders the dividend chart and chart title
+function renderDivChart(stockSymbol, annualizedDiv) {
+  $('.top-bar-title').text(`${stockSymbol.toUpperCase()}'s annualized dividend rate: ${annualizedDiv}%`);
+  $('.top-stock-dividend').width(Math.min(annualizedDiv, 100) + '%')
+                          .css("background-color", "mediumslateblue")
+                          .css("border", "1px solid mediumslateblue");
 }
 
-function renderSummaryText_DividendAnnualizedRate(stockSymbol, quoteOnDividendDate, annualizedDividend) {
-  $('.dividend-results-detail').append(`<br>${stockSymbol.toUpperCase()}'s stock quote on that date was: $${quoteOnDividendDate}<br>`);
-  $('.dividend-results-main').html(`<br>That represents an annualized return rate of: ${annualizedDividend}%<br>`);
+function renderDivRateText(stockSymbol, quoteOnDivDate, annualizedDiv) {
+  $('.dividend-results-detail').append(`-${stockSymbol.toUpperCase()}'s stock quote on that date was: $${quoteOnDivDate}<br>`);
+  // $('.dividend-results-main').html(`<br>That represents an annualized return rate of: ${annualizedDiv}%<br>`);
 }
 
-function renderSummaryText_DividendAmountAndDate(stockSymbol, dividendAmount, dividendDate) {
-  $('.dividend-results-detail').html(`${stockSymbol.toUpperCase()}'s most recent quarterly dividend amount was: $${dividendAmount}<br>
-    <br>${stockSymbol.toUpperCase()} declared that dividend on: ${dividendDate}<br>`);
+//Render the summary text for the dividend amount and date
+function renderText_DivAmtDate(stockSymbol, dividendAmount, dividendDate) {
+  $('.results-text-title').html('More details:');
+  $('.dividend-results-detail').html(`<br>-${stockSymbol.toUpperCase()}'s most recent quarterly dividend amount was: $${dividendAmount}<br>
+    -${stockSymbol.toUpperCase()} declared that dividend on: ${dividendDate}<br>`);
 }
 
 //This function handles the gathering, extraction and rendering of the non-dividend stock data
 function getOneYearStockGains(stockSymbol) {
   let stockQuoteJsonUrl = makeStockJsonUrl(stockSymbol);
-  $.getJSON(stockQuoteJsonUrl, data2 => { extractAndRenderNonDividendResults(data2, stockSymbol) });
+  $.getJSON(stockQuoteJsonUrl, data2 => { renderNonDivResults(data2, stockSymbol) });
 }
 
 //This is the callback function for the getJson - it handles non-dividend data extraction and results rendering
-function extractAndRenderNonDividendResults(data, stockSymbol) {
+function renderNonDivResults(data, stockSymbol) {
   let latestStockQuote = data[data.length-1].close; //the last element in the 1y array of objects
   let stockQuoteOneYearBack = data[0].close; //the first element in the 1y array of objects
   let annualGainNonDividend = oneYearGain(stockQuoteOneYearBack, latestStockQuote);
-  renderNonDividendResults(stockSymbol, annualGainNonDividend);
+  renderNonDivHtml(stockSymbol, annualGainNonDividend);
 }
 
 //This function handles the rendering of the non-dividend stock data
-function renderNonDividendResults(stockSymbol, annualGainNonDividend) {
-  $('.non-dividend-results').html(`<br>Compare that to ${stockSymbol.toUpperCase()}'s non-dividend gains over 
-    the past year (updated today): ${annualGainNonDividend}%`); 
+function renderNonDivHtml(stockSymbol, annualGainNonDividend) {
+  // $('.non-dividend-results').html(`<br>Compare that to ${stockSymbol.toUpperCase()}'s non-dividend gains over 
+  //   the past year (updated today): ${annualGainNonDividend}%`); 
   $('.middle-bar-title').text(`${stockSymbol.toUpperCase()}'s 1-year gain (non-dividend): ${annualGainNonDividend}%`);
   $('.middle-stock-non-dividend').width(Math.min(annualGainNonDividend, 100) + '%')
-                                .css("background-color", 'darkorange')
-                                .css("border", '1px solid black');  
+                                .css("background-color", 'dodgerblue')
+                                .css("border", '1px solid dodgerblue');  
 }
 
 //This function handles the gathering, extraction and rendering of the S&P-500 data
 function getOneYearSP500Gains() {
   let sp500QuoteJsonUrl = makeStockJsonUrl('VOO');
-  $.getJSON(sp500QuoteJsonUrl, data3 => { extractAndRenderSP500results(data3) });
+  $.getJSON(sp500QuoteJsonUrl, data3 => { renderSP500results(data3) });
 }
 
-function extractAndRenderSP500results(data) {
+function renderSP500results(data) {
   let latestSP500Quote = data[data.length-1].close; //the last element in the 1y array of objects
   let sp500QuoteOneYearBack = data[0].close; //the first element in the 1y array of objects
   let sp500AnnualGain = oneYearGain(sp500QuoteOneYearBack, latestSP500Quote);
-  renderSP500Results(sp500AnnualGain);
+  renderSP500Html(sp500AnnualGain);
 }
 
 //This function handles the rendering of the S&P-500 data
-function renderSP500Results(sp500AnnualGain) {
-  $('.sp500-results').html(`<br>Also, compare that to the S&P-500 gains over 
-    the past year (updated today): ${sp500AnnualGain}%`);  
+function renderSP500Html(sp500AnnualGain) {
+  // $('.sp500-results').html(`<br>Also, compare that to the S&P-500 gains over 
+  //   the past year (updated today): ${sp500AnnualGain}%`);  
   $('.bottom-bar-title').text(`S&P-500's 1-year gain: ${sp500AnnualGain}%`);
   $('.bottom-sp500').width(Math.min(sp500AnnualGain, 100) + '%') 
-                    .css("background-color", 'purple')
-                    .css("border", '1px solid black');
+                    .css("background-color", 'silver')
+                    .css("border", '1px solid silver');
 }
 
 function oneYearGain(startQuote, endQuote) {
@@ -142,7 +148,7 @@ function oneYearGain(startQuote, endQuote) {
 }
 
 //We use this function to clear the screen between runs
-function clearAllResultsFields() {
+function clearAllResultFields() {
   $('.logo').html('');
 
   $('.top-bar-title').html('');
@@ -155,6 +161,7 @@ function clearAllResultsFields() {
   $('.middle-stock-non-dividend').html('');
   $('.bottom-sp500').html('');
 
+  $('.results-text-title').html('');
   $('.dividend-results-detail').html('');
   $('.dividend-results-main').html('');
   $('.non-dividend-results').html('');
@@ -163,28 +170,28 @@ function clearAllResultsFields() {
   $('.results-text').css("border", "0");
 }
 
-function clearManualInputField() {
+function clearInputField() {
   $('.stock-symbol-input').val('');
 }
 
 //Finally, we need to setup two listeners:
-function listenForFreeTextSubmission() {
+function listenForFreeTextSubmit() {
   $('.input-form').submit(event => {
     event.preventDefault();
-    clearAllResultsFields();
+    clearAllResultFields();
     const userInput = $(event.currentTarget).find('.stock-symbol-input');
     const userStockSymbol = userInput.val();
-    getDataAndRenderSummary(userStockSymbol);
+    getDataRenderSummary(userStockSymbol);
   });
 }
 
-function listenForFixedButtonSubmission() {
+function listenForFixedButtonSubmit() {
   $('.fixed-button').click(event => {
-    clearManualInputField(); //we need this 
-    clearAllResultsFields();
+    clearInputField(); //we need this 
+    clearAllResultFields();
     event.preventDefault();
     const userStockSymbol = $(event.currentTarget).text();
-    getDataAndRenderSummary(userStockSymbol);
+    getDataRenderSummary(userStockSymbol);
   });
 }
 
@@ -193,5 +200,5 @@ function renderLogo(stockSymbol) {
   $(".logo").html('<img src="' + logoUrl + '" alt="Selected company\'s URL" class="logo-img">');
 }
 
-$(listenForFreeTextSubmission);
-$(listenForFixedButtonSubmission);
+$(listenForFreeTextSubmit);
+$(listenForFixedButtonSubmit);
